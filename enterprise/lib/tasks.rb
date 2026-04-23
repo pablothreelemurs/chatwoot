e2941b6d94030e4e3907fdd35782f1e9ee04bfce
plamasflydevs <pablo.lamas@flydevs.com>
2026-04-23 10:19:25 -0300

    module Tasks

--- enterprise/lib/tasks.rb
+++ enterprise/lib/tasks.rb
@@ -1,6 +1,2 @@
-# Load all rake tasks from the enterprise/lib/tasks directory
 module Tasks
-  if defined?(Rake)
-    Dir.glob(File.join(File.dirname(__FILE__), 'tasks', '*.rake')).each { |r| load r }
-  end
 end

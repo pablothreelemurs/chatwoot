3bb103c7a56a41f691899678dccea3f118d1281c
plamasflydevs <pablo.lamas@flydevs.com>
2026-04-23 09:35:12 -0300

    Load all rake tasks from the enterprise/lib/tasks directory

--- enterprise/lib/tasks.rb
+++ enterprise/lib/tasks.rb
@@ -1,4 +1,6 @@
 # Load all rake tasks from the enterprise/lib/tasks directory
 module Tasks
-  Dir.glob(File.join(File.dirname(__FILE__), 'tasks', '*.rake')).each { |r| load r }
+  if defined?(Rake)
+    Dir.glob(File.join(File.dirname(__FILE__), 'tasks', '*.rake')).each { |r| load r }
+  end
 end
